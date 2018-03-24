4ad94db96b6e4f3dbb09aa46419c50a5fcb88262
akhileshsh026@hotmail.com <akhileshsh026@hotmail.com>
2018-03-24 21:46:36 +0530

    Created the data folder for accessing the smaple data so created the FriendsDataAccess class which contains the  IEnumerable Iterface Whose type is Firends from the Model project class , then it returs the objects as in the method after that extracted the Interface by pressing the ctl + . by seeting mouse over the class name .

---
+++ FriendOrganisor.UI/Data/FriendsDataService.cs
@@ -0,0 +1,21 @@
+﻿using FriendOrganisor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrganisor.UI.Data
+{
+    class FriendsDataService : IFriendsDataService
+    {
+        public IEnumerable<Friend> GetAllFriends()
+        {
+            // To Do Getting this data  from the Database after this thing.
+            yield return new Friend { Id = 1, FirstName = "Akhilesh Kumar", LastName = "Rajendra", Email = "akhileshsh026@hotmail.com" };
+            yield return new Friend { Id = 2, FirstName = "Arpit", LastName = "Kanda", Email = "Arpit.kanda@dimentionx.in" };
+            yield return new Friend { Id = 3, FirstName = "Surendra", LastName = "Chouksey", Email = "Surendra.Chouksey@dimentionx.in" };
+            
+        }
+    }
+}
---
+++ FriendOrganisor.UI/Data/IFriendsDataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using FriendOrganisor.Model;
+
+namespace FriendOrganisor.UI.Data
+{
+    interface IFriendsDataService
+    {
+        IEnumerable<Friend> GetAllFriends();
+    }
+}
