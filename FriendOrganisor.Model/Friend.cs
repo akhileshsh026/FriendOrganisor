6f78c8797d3a9a828925558efaf039ddcf7017f2
akhileshsh026@hotmail.com <akhileshsh026@hotmail.com>
2018-03-24 21:01:37 +0530

    Added the Contents of the model class

--- FriendOrganisor.Model/Friend.cs
+++ FriendOrganisor.Model/Friend.cs
@@ -8,6 +8,10 @@ namespace FriendOrganisor.Model
 {
     public class Friend
     {
-      
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
     }
 }
