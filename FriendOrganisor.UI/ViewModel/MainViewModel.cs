ad6f6e1bf512b41967e1d69021f11f50b624c21a
akhileshsh026@hotmail.com <akhileshsh026@hotmail.com>
2018-03-25 21:41:47 +0530

    Now we moved the ViewModelBase class sepratly in the proejct by pressing the shortcut key ctl+. by placing cursor to the end of the class.

--- FriendOrganisor.UI/ViewModel/MainViewModel.cs
+++ FriendOrganisor.UI/ViewModel/MainViewModel.cs
@@ -3,9 +3,7 @@ using FriendOrganisor.UI.Data;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -47,15 +45,6 @@ namespace FriendOrganisor.UI.ViewModel
 
       
 
-    }
-
-    public class ViewModelBase : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
-        }
     }
 
 }
---
+++ FriendOrganisor.UI/ViewModel/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FriendOrganisor.UI.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+
+}
