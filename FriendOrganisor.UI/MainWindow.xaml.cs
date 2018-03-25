6f4004c7301fd05cc92d9dd3295f1eac26a1ebb4
akhileshsh026@hotmail.com <akhileshsh026@hotmail.com>
2018-03-25 21:55:02 +0530

    we call the Instance of the MainViewModel in the constructor in of the MainWindowClass but we need to call the Load method it's not a good idea to cal l Load method in constructor .

--- FriendOrganisor.UI/MainWindow.xaml.cs
+++ FriendOrganisor.UI/MainWindow.xaml.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FriendOrganisor.UI.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FriendOrganisor.UI
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(MainViewModel viewmodel)
         {
             InitializeComponent();
+            DataContext = viewmodel;
         }
     }
 }
