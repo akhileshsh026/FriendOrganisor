d2a80e5c095fee05dbc1cbd5a0c4a5a6e574864b
akhileshsh026@hotmail.com <akhileshsh026@hotmail.com>
2018-03-26 08:47:54 +0530

    So we are using the Loaded+= event of the MainWindow and a MainModelView class obj. which is then used as a way to calling the Load() Method.

--- FriendOrganisor.UI/MainWindow.xaml.cs
+++ FriendOrganisor.UI/MainWindow.xaml.cs
@@ -6,11 +6,18 @@ namespace FriendOrganisor.UI
     
     public partial class MainWindow : Window
     {
+        private MainViewModel _viewmodel;
         public MainWindow(MainViewModel viewmodel)
         {
             InitializeComponent();
-            DataContext = viewmodel;
-            viewmodel.Load(); //bad idea
+            _viewmodel = viewmodel;
+            DataContext = _viewmodel;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _viewmodel.Load();
         }
     }
 }
